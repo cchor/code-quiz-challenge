5d40227276f62b19e9cfbcbd53417a2159520408
cchor <127997289+cchor@users.noreply.github.com>
2023-04-23 10:23:59 -0400

    added html to create the basis for the code quiz, css for styling to start, and javascript to add the start of the timer

---
+++ script.js
@@ -0,0 +1,9 @@
+const startingSeconds = .5;
+let time = startingSeconds * 60;
+
+const countDownEl = document.getElementById("countdown");
+
+function updateCountdown(){
+const seconds 
+    time--;
+}
