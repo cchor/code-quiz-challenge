91120309a522301e385e515adf1fcce6e2122bae
cchor <127997289+cchor@users.noreply.github.com>
2023-04-26 17:04:06 -0400

    updating changes

--- game.js
+++
@@ -1,5 +0,0 @@
-const question = document.getElementById("question");
-const el = document.getElementById('el');
-// const choices = document.getElementsByClassName("choice-text");
-const choices = Array.from(document.getElementsByClassName("choice-text"));
-console.log(choices);
