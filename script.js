f6f21a7f34e8c9b734aff91ce38fd884cc4e3f9c
cchor <127997289+cchor@users.noreply.github.com>
2023-04-29 02:43:49 -0400

    still working on game

--- script.js
+++ script.js
@@ -7,38 +7,38 @@ var questionIndex = 0;
 
 var questionArray = [
     {
-        question: "What are variables used for?",
-        options: ["Placeholders for information", "To call a function", "They are operators", "All of the above"],
-        correctAns: "Placeholders for information",
+        title: "What are variables used for?",
+        choices: ["Placeholders for information", "To call a function", "They are operators", "All of the above"],
+        answer: "Placeholders for information",
     },
     {
-        question: "What do loops do?",
-        options: ["They compare variables", "Make content go in a circle", "Work as a repeated IF statement", "All of the above"],
-        correctAns: "Work as a repeated IF statement",
+        title: "What do loops do?",
+        choices: ["They compare variables", "Make content go in a circle", "Work as a repeated IF statement", "All of the above"],
+        answer: "Work as a repeated IF statement",
     },
     {
-        question: "What are functions??",
-        options: ["Placeholders for information", "They identify a specific value", "A block of code to perform a task", "All of the above"],
-        correctAns: "A block of code to perform a task",
+        title: "What are functions??",
+        choices: ["Placeholders for information", "They identify a specific value", "A block of code to perform a task", "All of the above"],
+        answer: "A block of code to perform a task",
     },
     {
-        question: "What is an event listener?",
-        options: ["A method that records events", "A tool that listens to the user", "Work as a repeated IF statement", "A method that waits for a specific event to occur"],
-        correctAns: "A method that waits for a specific event to occur",
+        title: "What is an event listener?",
+        choices: ["A method that records events", "A tool that listens to the user", "Work as a repeated IF statement", "A method that waits for a specific event to occur"],
+        answer: "A method that waits for a specific event to occur",
     }
 
 ]
 
-function renderQuestion() {
-    var questionArray = questionEl[questionIndex];
-    var questionEl = document.getElementById('question.title');
+function getQuestion() {
+    var questionArray = question[questionIndex];
+    var question = document.getElementById('question');
     questionEl.textContent = questionArray.title;
     correctAnsEl.innerHTML = '';
 
     for (var i = 0; i < questionArray.options.length; i++) {
-    var questAns = questionArray.options[0];
+    var correctAns = questionIndex.options[0];
     var choiceNode = document.createElement('button');
-    choiceNode.setAttribute('answer-button', 'options');
+    choiceNode.setAttribute('answer-button', 'questAns');
     choiceNode.setAttribute('value', options);
     choiceNode.textContent = i + 1 +'.' + options;
     answerEl.appendChild(choiceNode);
