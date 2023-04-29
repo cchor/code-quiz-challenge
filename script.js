e3930f9656e0087138e3e8ecab79f6442e69725f
cchor <127997289+cchor@users.noreply.github.com>
2023-04-29 00:37:02 -0400

    updated answers and put them into an array

--- script.js
+++ script.js
@@ -6,50 +6,58 @@ var startContainer=document.querySelector(".start-container");
 var questionIndex = 0;
 
 var questionArray = [
-    // {
-    //     question: "What are variables used for?",
-    //     answers: ["Placeholders for information", "To call a function","They are operators","All of the above"]
-    //     correctAnswer: "Placeholders for information"
-    // },
-
     {
         question: "What are variables used for?",
-        answer1: "Placeholders for information",
-        answer2: "To call a function",
-        answer3: "They are operators",
-        answer4: "All of the above",
-        correctAnswer: "Placeholders for information",
+        options: ["Placeholders for information", "To call a function", "They are operators", "All of the above"],
+        correctAns: "Placeholders for information",
     },
     {
         question: "What do loops do?",
-        answer1: "They compare variables",
-        answer2: "Make content go in a circle",
-        answer3: "Work as a repeated IF statement",
-        answer4: "All of the above",
-        correctAnswer: "Work as a repeated IF statement"
+        options: ["They compare variables", "Make content go in a circle", "Work as a repeated IF statement", "All of the above"],
+        correctAns: "Work as a repeated IF statement",
     },
     {
-        question: "What are functions?",
-        answer1: "Placeholders for information",
-        answer2: "They identify a specific value",
-        answer3: "A block of code to perform a task",
-        answer4: "All of the above",
-        correctAnswer: "A block of code to perform a task"
+        question: "What are functions??",
+        options: ["Placeholders for information", "They identify a specific value", "A block of code to perform a task", "All of the above"],
+        correctAns: "A block of code to perform a task",
     },
     {
         question: "What is an event listener?",
-        answer1: "A method that records events",
-        answer2: "A tool that listens to the user",
-        answer3: "A method that waits for a specific event to occur",
-        answer4: "All of the above",
-        correctAnswer: "A method that waits for a specific event to occur"
+        options: ["A method that records events", "A tool that listens to the user", "Work as a repeated IF statement", "A method that waits for a specific event to occur"],
+        correctAns: "A method that waits for a specific event to occur",
     }
+
 ]
 
-const question = document.getElementById("question");
-const answers = Array.from(document.getElementsByClassName("answer-button"));
-console.log(answers);
+function renderQuestion() {
+    var questionArray = questionEl[questionIndex];
+    var questionEl = document.getElementById('question.title');
+    questionEl.textContent = questionArray.title;
+    correctAnsEl.innerHTML = '';
 
+    for (var i = 0; i < questionArray.options.length; i++) {
+    var questAns = questionArray.options[0];
+    var choiceNode = document.createElement('button');
+    choiceNode.setAttribute('answer-button', 'options');
+    choiceNode.setAttribute('value', options);
+    choiceNode.textContent = i + 1 +'.' + options;
+    answerEl.appendChild(choiceNode);
+
+    }
+}
+    console.log(questionArray);
+
+
+function startGame() {
+    startContainer.style.display="none";
+    gameContainer.style.display="flex";
+    questionCounter = 0;
+    score = 0;
+    timerCountDown();
+    renderQuestion();
+}
+
+startBtn.addEventListener("click",startGame);
 
 
 // countdown timer
@@ -63,27 +71,3 @@ function timerCountDown() {
 }}, 1000)
 
 }
-
-function renderQuestion() {
-    document.getElementById("question").textContent = questionArray[questionIndex].question;
-    document.getElementById("answer1").textContent = questionArray[questionIndex].answer1;
-    document.getElementById("answer2").textContent = questionArray[questionIndex].answer2;
-    document.getElementById("answer3").textContent = questionArray[questionIndex].answer3;
-    document.getElementById("answer4").textContent = questionArray[questionIndex].answer4;
-    for (let i = 0; i < answer; i++) {
-        
-    }
-}
-
-
-
-function startGame() {
-    startContainer.style.display="none";
-    gameContainer.style.display="flex";
-    questionCounter = 0;
-    score = 0;
-    timerCountDown();
-    renderQuestion();
-}
-
-startBtn.addEventListener("click",startGame);
